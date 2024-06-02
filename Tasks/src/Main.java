7d78d126bccddc0ed9651b272407ccdc606dd233
SonGonHan <miskasabancik@gmail.com>
2024-06-02 17:42:23 +0300

    1.2
    Реализуйте алгоритм сортировки пузырьком для сортировки массива

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        final int N = 100;
+        final int N = 10;
         var Arr = new double[N];
-        double min_val = 1, max_val = 0, avg_val = 0;
         for (int i = 0; i < N; ++i) {
             Arr[i] = Math.random();
-            min_val = Math.min(min_val, Arr[i]);
-            max_val = Math.max(max_val, Arr[i]);
-            avg_val += Arr[i] / N;
         }
-        System.out.println("Min val: " + min_val);
-        System.out.println("Max val: " + max_val);
-        System.out.println("Avg val: " + avg_val);
+        double swap;
+        for (int i = 0; i < N; ++i) {
+            for (int j = N - 1; j > i; --j) {
+                if (Arr[j - 1] > Arr[j]) {
+                    swap = Arr[j - 1];
+                    Arr[j - 1] = Arr[j];
+                    Arr[j] = swap;
+                }
+            }
+        }
+        for (var x : Arr) {
+            System.out.println(x);
+        }
     }
 }
