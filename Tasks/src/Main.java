03281c5d55851619a849841af587042b40abb444
SonGonHan <miskasabancik@gmail.com>
2024-06-03 17:33:17 +0300

    1.4
    Дан массив целых чисел и ещё одно целое число. Удалите все вхождения этого числа из массива (пропусков быть не должно).

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        final int N = 100;
-        int i, j;
-        find_simple:
-        for (i = 2; i < N; ++i) {
-            for (j = 2; j <= Math.sqrt(i); ++j) {
-                if (i % j == 0) {
-                    continue find_simple;
-                }
+
+    public static int N = 10, RANGE = 10;
+
+    private static int[] remove(int[] Arr, int num_del) {
+        int remove_count = 0;
+        for (int i = 0; i < N - remove_count; ++i) {
+            if (Arr[i] == num_del) {
+                Arr[i] = Arr[N - (++remove_count)];
             }
-            System.out.println(i);
         }
+        return Arrays.copyOf(Arr, N - remove_count);
     }
+
+    public static void main(String[] args) {
+        int[] Arr = new int[N];
+        for (int i = 0; i < N; ++i) {
+            Arr[i] = (int) Math.floor(Math.random() * RANGE);
+        }
+        System.out.println(Arrays.toString(Arr));
+
+        int num_del = (int) Math.floor(Math.random() * RANGE);
+        System.out.println(num_del);
+        
+        System.out.println(Arrays.toString(remove(Arr, num_del)));
+    }
+
 }
