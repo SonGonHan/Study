eb727ae5f291d6657a637a60464aadb4fd1fdd0c
SonGonHan <miskasabancik@gmail.com>
2024-06-08 14:17:01 +0300

    5.4
    Напишите метод, возвращающий количество вхождений одной строки в другую.

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,15 +6,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String path = "D:/Projects/Java/Study/Tasks/text.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        String Text = "", Line;
-        while ((Line = reader.readLine()) != null) {
-            Text += Line + " ";
+        Scanner reader = new Scanner(new File(path));
+        String Text = "";
+        while (reader.hasNextLine()) {
+            Text += reader.nextLine() + " ";
         }
+        reader.close();
         Scanner console = new Scanner(System.in);
         System.out.print("Enter word: ");
-        String Word = console.nextLine();
-        Text = Text.replaceAll(Word, "***");
-        System.out.println(Text);
+        String found_word = (console.nextLine()).toLowerCase().strip();
+        String[] words = Text.split(" ");
+        int count = 0;
+        for (String word : words) {
+            word = word.replaceAll("\\pP", "").toLowerCase().strip();
+            System.out.println(found_word + " " + word);
+            if (word.equals(found_word)) {
+                ++count;
+            }
+        }
+        System.out.println(count);
+        console.close();
     }
 }
