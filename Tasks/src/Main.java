f57d9740bc70208da325a0c262f4b382c69b7968
SonGonHan <miskasabancik@gmail.com>
2024-06-08 14:06:13 +0300

    5.3
    Напишите метод, заменяющий в тексте все вхождения вводимого слова на "***"

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,22 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static String check(String word) {
-        for (int i = 0; i < word.length() / 2; ++i) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-                return "No";
-            }
+    public static void main(String[] args) throws IOException {
+        String path = "D:/Projects/Java/Study/Tasks/text.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String Text = "", Line;
+        while ((Line = reader.readLine()) != null) {
+            Text += Line + " ";
         }
-        return "Yes";
-    }
-
-    public static void main(String[] args) {
-        Scanner console_in = new Scanner(System.in);
-        String word;
-        while (!(word = console_in.nextLine()).isEmpty()) {
-            System.out.println(check(word));
-        }
-        console_in.close();
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter word: ");
+        String Word = console.nextLine();
+        Text = Text.replaceAll(Word, "***");
+        System.out.println(Text);
     }
 }
