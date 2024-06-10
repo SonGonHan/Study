2d412c82a7cc9d2a92500f10d580b5979afd7b64
SonGonHan <miskasabancik@gmail.com>
2024-06-10 16:26:07 +0300

    6.4_1
    То же самое, что и предыдущее, только с помощью Generic

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Main {
 
-    public static Object[] filter(Object[] Arr, Predicate<Object> filter) {
+    public static <T> T[] filter(T[] Arr, Predicate<T> filter) {
         int offset = 0;
         for (int i = 0; i < Arr.length - offset; ++i) {
             if (filter.test(Arr[i])) {
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         String[] array = new String[]{"1rewf ", "feefewf", "a", null, ";l;"};
 
-        String[] newArray = (String[]) filter(array, Objects::isNull);
+        String[] newArray = filter(array, Objects::isNull);
         System.out.println(Arrays.toString(newArray));
     }
 }
