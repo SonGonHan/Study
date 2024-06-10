6ba9db1f81802c32a1bc77a2a275d5748567def9
SonGonHan <miskasabancik@gmail.com>
2024-06-10 17:13:35 +0300

    7.1
    Сравнение ArrayList и LinkedList

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,17 +1,36 @@
-import java.util.Arrays;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
-    public static <T> void fill(T[] Arr, Function<Integer, T> f) {
-        for (int i = 0; i < Arr.length; ++i) {
-            Arr[i] = f.apply(i);
-        }
-    }
-
     public static void main(String[] args) {
-        Integer[] squares = new Integer[10];
-        fill(squares, integer -> integer * integer);
-        System.out.println(Arrays.toString(squares));
+        final int N = 1_000_000;
+        ArrayList<Double> AL= new ArrayList<>();
+        LinkedList<Double> LL= new LinkedList<>();
+
+        var time = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            AL.add(Math.random());
+        }
+        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
+
+        time = System.nanoTime();
+        for (int i = 0; i < N; ++i) {
+            LL.add(Math.random());
+        }
+        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
+
+        double val;
+        time = System.nanoTime();
+        for (int i = 0; i < N; ++i) {
+            val = AL.get((int)Math.floor(Math.random() * N));
+        }
+        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
+
+        time = System.nanoTime();
+        for (int i = 0; i < N; ++i) {
+            val = LL.get((int)Math.floor(Math.random() * N));
+        }
+        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
     }
 }
