45f0837896dc544a82e947619ecfa70b3d3cef5f
SonGonHan <miskasabancik@gmail.com>
2024-06-11 13:18:01 +0300

    7.2
    Написать итератор по массиву

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,36 +1,60 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Main {
 
+    public static class ArrayIterator <T> implements Iterator<T> {
+
+        private final T[] Array;
+        private int index;
+
+        ArrayIterator(T[] Array) {
+            this.Array = Array;
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index + 1 < Array.length;
+        }
+
+        public boolean hasPrevious() {
+            return index > 0;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Нет следующего элемента");
+            }
+            return Array[++index];
+        }
+
+        public T previous() {
+            if (!hasPrevious()) {
+                throw new NoSuchElementException("Нет предыдущего элемента");
+            }
+            return Array[--index];
+        }
+
+        public int getIndex() {
+            return index;
+        }
+    }
+
     public static void main(String[] args) {
-        final int N = 1_000_000;
-        ArrayList<Double> AL= new ArrayList<>();
-        LinkedList<Double> LL= new LinkedList<>();
-
-        var time = System.nanoTime();
-        for (int i = 0; i < N; i++) {
-            AL.add(Math.random());
+        Integer[] Array = {1, 2, 3, 4};
+        ArrayIterator<Integer> it = new ArrayIterator<>(Array);
+        for (int i = 0; i < 3; ++i) {
+            System.out.println(it.next());
         }
-        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
-
-        time = System.nanoTime();
-        for (int i = 0; i < N; ++i) {
-            LL.add(Math.random());
+        System.out.println();
+        System.out.println(it.getIndex());
+        System.out.println();
+        for (int i = 0; i < 3; ++i){
+            System.out.println(it.previous());
+            System.out.println();
         }
-        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
-
-        double val;
-        time = System.nanoTime();
-        for (int i = 0; i < N; ++i) {
-            val = AL.get((int)Math.floor(Math.random() * N));
-        }
-        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
-
-        time = System.nanoTime();
-        for (int i = 0; i < N; ++i) {
-            val = LL.get((int)Math.floor(Math.random() * N));
-        }
-        System.out.println((System.nanoTime() - time) / 1000000 + " ms");
+        System.out.println(it.getIndex());
     }
 }
