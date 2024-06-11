39a5ec7f528bfa71a67e9bcaf401f1527621bbc7
SonGonHan <miskasabancik@gmail.com>
2024-06-11 14:13:49 +0300

    7.3
    Написать итератор по двусвязному списку

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,25 +1,23 @@
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 
 public class Main {
 
-    public static class ArrayIterator <T> implements Iterator<T> {
+    public static class MyListIterator<T> implements ListIterator<T> {
 
-        private final T[] Array;
+        private final LinkedList<T> List;
         private int index;
 
-        ArrayIterator(T[] Array) {
-            this.Array = Array;
-            index = 0;
+        MyListIterator(LinkedList<T> List) {
+            this.List = List;
+            index = -1;
         }
 
         @Override
         public boolean hasNext() {
-            return index + 1 < Array.length;
-        }
-
-        public boolean hasPrevious() {
-            return index > 0;
+            return index + 1 < List.size();
         }
 
         @Override
@@ -27,34 +25,82 @@ public class Main {
             if (!hasNext()) {
                 throw new NoSuchElementException("Нет следующего элемента");
             }
-            return Array[++index];
+            return List.get(++index);
         }
 
+        @Override
+        public boolean hasPrevious() {
+            return index > 0;
+        }
+
+        @Override
         public T previous() {
             if (!hasPrevious()) {
-                throw new NoSuchElementException("Нет предыдущего элемента");
+                throw new NoSuchElementException("Нет предшествующего элемента");
             }
-            return Array[--index];
+            return List.get(--index);
         }
 
-        public int getIndex() {
-            return index;
+        @Override
+        public int nextIndex() {
+            return index + 1;
+        }
+
+        @Override
+        public int previousIndex() {
+            return index - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (index < 0 || index >= List.size()) {
+                throw new IllegalStateException("Нельзя удалить элемент");
+            }
+            List.remove(index);
+        }
+
+        @Override
+        public void set(T t) {
+            if (index < 0 || index >= List.size()) {
+                throw new IllegalStateException("Нельзя изменить элемент");
+            }
+
+            List.set(index, t);
+        }
+
+        @Override
+        public void add(T t) {
+            if (index < 0 || index > List.size()) {
+                throw new IllegalStateException("Нельзя вставить элемент");
+            }
+            List.add(index, t);
         }
     }
 
     public static void main(String[] args) {
-        Integer[] Array = {1, 2, 3, 4};
-        ArrayIterator<Integer> it = new ArrayIterator<>(Array);
-        for (int i = 0; i < 3; ++i) {
-            System.out.println(it.next());
+        LinkedList<Integer> LL= new LinkedList<>();
+        LL.add(1);
+        LL.add(2);
+        LL.add(3);
+        LL.add(4);
+        MyListIterator<Integer> it = new MyListIterator<>(LL);
+        for (int i = 0; i < LL.size(); ++i) {
+            System.out.print(it.next() + " ");
         }
         System.out.println();
-        System.out.println(it.getIndex());
-        System.out.println();
-        for (int i = 0; i < 3; ++i){
-            System.out.println(it.previous());
-            System.out.println();
+        it.set(1000);
+        for (int i = 0; i < 2; ++i) {
+            System.out.print(it.previous() + " ");
         }
-        System.out.println(it.getIndex());
+        System.out.println();
+        it.remove();
+        it = new MyListIterator<>(LL);
+        for (int i = 0; i < LL.size(); ++i) {
+            System.out.print(it.next() + " ");
+        }
+        System.out.println();
+        it.add(-1);
+        it.previous();
+        System.out.print(it.next());
     }
 }
